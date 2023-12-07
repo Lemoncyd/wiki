3988358529842c3c3e3992e4b1a121950ec2b9ad
unknown <chenyudong@uascent-iot.com>
2023-12-07 16:38:30 +0800

    BK3432_DesignKit_V17_1510 1、 修改 ble_app_gatt_scan_advertising 工程不能被链接问题 2、修改 ble_app_gatt_multi_connect 工程链接多个设备出现的 异常问题

---
+++ SDK/del.bat
@@ -0,0 +1,24 @@
+del Untitled*.*  /s/q
+del ..\*.docx  /s/q
+rd projects\ble_stack /s /q
+rd projects\ble_stack_1 /s /q
+rd projects\ble_stack_scan_adv /s /q
+rd projects\ble_stack_multi_connect /s /q
+rd projects\rf_test /s /q
+rd sdk\ble_stack\src /s /q
+rd projects\ble_app_ancs\obj /s /q
+rd projects\ble_app_gatt\obj /s /q
+rd projects\ble_app_gatt_multi_connect\obj /s /q
+rd projects\ble_app_gatt_1\obj /s /q
+rd projects\ble_app_gatt_128\obj /s /q
+rd projects\ble_app_gatt_long_pdu_advertising\obj /s /q
+rd projects\ble_app_gatt_muti_chars\obj /s /q
+rd projects\ble_app_gatt_scan_advertising\obj /s /q
+rd projects\ble_app_gma_1\obj /s /q
+rd projects\ble_app_LLSync_1\obj /s /q
+rd projects\ble_app_remote\obj /s /q
+rd projects\ble_app_wechat\obj /s /q
+rd projects\ble_app_gatt_sm\obj /s /q
+del ..\..\bk3432_V*.rar  /s/q
+
+
---
+++ SDK/projects/ble_app_gatt_scan_advertising/output/encrypt_app.bat
@@ -0,0 +1,2 @@
+.\output\BinConvert_3432.exe  -oad .\output\bim\bk3432_ble_bim.bin  .\output\stack\bk3432_ble_stack.bin  .\output\app\bk3432_ble_app.bin -m 0x1020 -l 0x1bf20 -v 0x0015 -rom_v 0x0010 -e 00000000 00000000 00000000 00000000
+
